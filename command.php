d79071f980ba059563dad769c7086bb4da196380
Adrian Garner <adrigen@gmail.com>
2015-08-11 19:59:33 +1000

    First commit

---
+++ command.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Changes WP-Piwik settings.
+ */
+class Piwik_Command extends WP_CLI_Command {
+
+    /**
+     *  Set piwik url.
+     * 
+     * ## OPTIONS
+     * 
+     * <url>
+     * : Enter your Piwik URL. This is the same URL you use to access your Piwik instance, e.g. http://www.example.com/piwik/
+     * 
+     * ## EXAMPLES
+     * 
+     *     wp example url http://www.example.com/piwik/ 
+     *
+     * @synopsis <url>
+     */
+    function url( $args, $assoc_args ) {
+        list( $url ) = $args;
+        update_site_option ( 'wp-piwik_global-piwik_url', $url );
+        // Print a success message
+        WP_CLI::success( "Url set to: $url" );
+    }
+    /**
+     * Set auth token.
+     * 
+     * ## OPTIONS
+     * 
+     * <token>
+     * : Enter your Piwik auth token here. It is an alphanumerical code like 0a1b2c34d56e78901fa2bc3d45678efa (see WP-Piwik faq for more info)
+     * 
+     * ## EXAMPLES
+     * 
+     *     wp piwik token 0a1b2c34d56e78901fa2bc3d45678efa
+     *
+     * @synopsis <token>
+     */
+    function token( $args, $assoc_args ) {
+        list( $token ) = $args;
+        update_site_option ( 'wp-piwik_global-piwik_token', $token );
+        // Print a success message
+        WP_CLI::success( "Auth token set to: $token" );
+    }
+}
+
+WP_CLI::add_command( 'piwik', 'Piwik_Command' );
